9f5344475017400c8dc650619fac2a02ffb8a5a7
hanastrickland <strickh@uw.edu>
2018-05-11 22:14:50 -0700

    create new index.Rmd, add markdown write up for summary stats

--- a7.R
+++ a7.R
@@ -14,6 +14,34 @@ exp_by_info_interest <- survey_data %>%
             maximum = max(programming_exp, na.rm = TRUE)
             )
 
+median_no_info <- exp_by_info_interest %>% 
+  filter(info_interest == "No") %>% 
+  select(median)
+
+median_yes_info <- exp_by_info_interest %>% 
+  filter(info_interest == "Yes") %>% 
+  select(median)
+
+median_unsure_info <- exp_by_info_interest %>% 
+  filter(info_interest == "Not sure") %>% 
+  select(median)
+
+mean_no_info <- exp_by_info_interest %>% 
+  filter(info_interest == "No") %>% 
+  select(mean) %>% 
+  round(2)
+
+mean_yes_info <- exp_by_info_interest %>% 
+  filter(info_interest == "Yes") %>% 
+  select(mean) %>% 
+  round(2)
+
+
+mean_unsure_info <- exp_by_info_interest %>% 
+  filter(info_interest == "Not sure") %>% 
+  select(mean) %>% 
+  round(2)
+
 exp_by_os <- survey_data %>% 
   filter(os %in% c("Windows", "Linux (any)", "Mac OS X")) %>% 
   group_by(os) %>%  
@@ -22,3 +50,31 @@ exp_by_os <- survey_data %>%
             minimum = min(programming_exp, na.rm = TRUE),
             maximum = max(programming_exp, na.rm = TRUE)
   )
+
+
+median_mac <- exp_by_os %>% 
+  filter(os == "Mac OS X") %>% 
+  select(median)
+
+median_windows <- exp_by_os %>% 
+  filter(os == "Windows") %>% 
+  select(median)
+
+median_linux <- exp_by_os %>% 
+  filter(os == "Linux (any)") %>% 
+  select(median)
+
+mean_mac <- exp_by_os %>% 
+  filter(os == "Mac OS X") %>% 
+  select(mean) %>% 
+  round(2)
+
+mean_windows <- exp_by_os %>% 
+  filter(os == "Windows") %>% 
+  select(mean) %>% 
+  round(2)
+
+mean_linux <- exp_by_os %>% 
+  filter(os == "Linux (any)") %>% 
+  select(mean) %>% 
+  round(2)
