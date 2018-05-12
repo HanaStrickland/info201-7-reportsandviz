195007f2d4d735119d745735daf017ca1e6130d5
hanastrickland <strickh@uw.edu>
2018-05-11 21:43:53 -0700

    add summaries for programming experience by info_interest and os, also correct survey_data to include stringsAsFactors = FALSE

--- a7.R
+++ a7.R
@@ -1,1 +1,24 @@
-survey_data <- read.csv("data/intro_survey.csv")
+library(dplyr)
+survey_data <- read.csv("data/intro_survey.csv", stringsAsFactors = FALSE)
+
+
+
+###
+### Summary Statistics
+###
+exp_by_info_interest <- survey_data %>% 
+  group_by(info_interest) %>%  
+  summarize(mean = mean(programming_exp, na.rm = TRUE), 
+            median = median(programming_exp,na.rm = TRUE),
+            minimum = min(programming_exp, na.rm = TRUE),
+            maximum = max(programming_exp, na.rm = TRUE)
+            )
+
+exp_by_os <- survey_data %>% 
+  filter(os %in% c("Windows", "Linux (any)", "Mac OS X")) %>% 
+  group_by(os) %>%  
+  summarize(mean = mean(programming_exp, na.rm = TRUE), 
+            median = median(programming_exp,na.rm = TRUE),
+            minimum = min(programming_exp, na.rm = TRUE),
+            maximum = max(programming_exp, na.rm = TRUE)
+  )
